bd3bed979a36cb6059142e76363d168169026512
wallrony <wrony.neves@gmail.com>
2022-11-10 09:32:18 -0300

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Rony on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
